0c24a200404438cc791d4643096d7b7be1db3120
Yaroslav Smirnov <stellarnode@ya.ru>
2016-08-31 19:15:28 +0300

    Initial Commit

---
+++ TcehProject/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TcehProject
+//
+//  Created by Yaroslav Smirnov on 31/08/2016.
+//  Copyright © 2016 Yaroslav Smirnov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TcehProject/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TcehProject
+//
+//  Created by Yaroslav Smirnov on 31/08/2016.
+//  Copyright © 2016 Yaroslav Smirnov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
